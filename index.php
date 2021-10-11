cdbbb01d5f36936eb46d2175dbc42b1e38308006
aurelBichop <linux.aurelien@gmail.com>
2021-10-11 14:30:15 +0200

    ajout du container et passage des services par références

--- index.php
+++ index.php
@@ -4,13 +4,58 @@ use App\Controller\OrderController;
 use App\Database\Database;
 use App\Mailer\GmailMailer;
 use App\Texter\SmsTexter;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$database = new Database();
-$texter = new SmsTexter("service.sms.com", "apikey123");
-$mailer = new GmailMailer("lior@gmail.com", "123456");
-$controller = new OrderController($database, $mailer, $texter);
+$container = new ContainerBuilder();
+
+
+$controllerDefinition = new Definition(OrderController::class,[
+    new Reference('database'),
+    new Reference('mailer.gmail'),
+    new Reference('texter.sms'),
+]);
+
+
+
+$databaseDefinition = new Definition(Database::class);
+$container->setDefinition('database',$databaseDefinition);
+//$container->set('database',new Database());
+
+$smsTexterDefinition = new Definition(SmsTexter::class,[
+    "service.sms.com",
+    "apikey123"
+]);
+// $smsTexterDefinition
+//     ->addArgument("service.sms.com")
+//     ->addArgument("apikey123");
+
+// $smsTexterDefinition->setArguments([
+//     "service.sms.com",
+//     "apikey123"
+// ]);
+$container->setDefinition('texter.sms',$smsTexterDefinition);
+
+$gmailMailerDefinition = new Definition(GmailMailer::class,[
+    "lior@gmail.com",
+    "123456"
+]);
+$container->setDefinition('mailer.gmail',$gmailMailerDefinition);
+
+// $database = $container->get('database');
+// $texter = $container->get('texter.sms');
+// $mailer = $container->get('mailer.gmail');
+
+
+$container->setDefinition('order_controller',$controllerDefinition);
+
+$controller = $container->get('order_controller');
+
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 
